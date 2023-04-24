28b41111353992f84605c018c3256593ec2e1f01
BohdanC <55244228+BohdanC@users.noreply.github.com>
2023-04-24 16:37:34 +0300

    'hw11'

--- HomeWork11/test_hw11/test_hw11_function.py
+++ HomeWork11/test_hw11/test_hw11_function.py
@@ -1,5 +1,4 @@
 import pytest
-
 import HomeWork11.hw11
 
 
@@ -23,16 +22,18 @@ testcases_error = [
     (set, TypeError),
 ]
 
+
 @pytest.mark.parametrize('writen_string, expected', testcases_positive)
 def test_positive_cases(writen_string, expected):
     assert HomeWork11.hw11.clean_string(writen_string) == expected, 'something went wrong'
+
 
 @pytest.mark.parametrize('writen_string, unexpected', testcases_negative)
 def test_negative_cases(writen_string, unexpected):
     assert HomeWork11.hw11.clean_string(writen_string) != unexpected
 
+
 @pytest.mark.parametrize('writen_string, expected_error', testcases_error)
 def test_error_cases(writen_string, expected_error):
     with pytest.raises(expected_error):
         HomeWork11.hw11.clean_string(writen_string)
-

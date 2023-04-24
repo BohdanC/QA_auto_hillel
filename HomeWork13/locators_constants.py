d1149ae091e62580e8891403b26b678bf5c7bf35
BohdanC <55244228+BohdanC@users.noreply.github.com>
2023-04-24 17:59:42 +0300

    'hw13'

--- HomeWork13/locators_constants.py
+++ HomeWork13/locators_constants.py
@@ -2,5 +2,5 @@ class LocatorsConstants:
     BOT_CHECK_XPATH = "//input[@type='checkbox']"
     EMAIL_FIELD_XPATH = "//input[@class='email valid']"
     PASSWORD_FIELD_XPATH = "//input[@class='password valid']"
-    SUBMIT_BUTTON_XPATH = "//span[@class='mark']"
+    SUBMIT_BUTTON_XPATH = "//button[@type='submit']"
     LOGGED_IN_CHECK_XPATH = "//li/a[text()='John Smith']"
